c2ebed87de57d6bd0452aea32ba951557a842cb7
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-11-02 13:42:45 +1000

    Начало

--- Wpf_Yshakow_PR3.1INS/MainWindow.xaml.cs
+++ Wpf_Yshakow_PR3.1INS/MainWindow.xaml.cs
@@ -23,6 +23,31 @@ namespace Wpf_Yshakow_PR3._1INS
         public MainWindow()
         {
             InitializeComponent();
+
+            text1.Text = "0";
+            text2.Text = "0";
+            text3.Text = "0";
+        }
+
+        float a, b;
+        int count;
+
+        private void bt1_Click_1(object sender, RoutedEventArgs e)
+        {
+            a = float.Parse(text1.Text);
+            count = 1;
+            calculate();
+        }
+
+        private void bt19_Click(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "";
+            text2.Text = "";
+            text3.Text = "";
+        }
+
+        private void calculate()
+        {
         }
     }
 }
